42a566b759e039fb6727b29d94a1e1f4a2c77c09
Vicente Rivera <vicenterivera225@gmail.com>
2023-08-23 21:22:23 -0400

    login a medias

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import HomePage from './pages/HomePage.jsx';
-import Jobs from './pages/JobsPage.jsx';
-import DrsPage from './pages/Drspage.jsx';
-import NotFound from './pages/NotFound.jsx';
-import MyCalendar from './pages/CalendarPage.jsx';
-import NavBar from './components/Header/NavBar.jsx';
-import Login from "./pages/Login.jsx"
-import Register from "./pages/Register.jsx"
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import Jobs from "./pages/JobsPage.jsx";
+import DrsPage from "./pages/Drspage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import MyCalendar from "./pages/CalendarPage.jsx";
+import NavBar from "./components/Header/NavBar.jsx";
+import Login from "./pages/LoginPage.jsx";
+import Register from "./pages/RegisterPage.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 export default function App() {
   return (
-    <Router>
-      <NavBar/>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/jobs" element={<Jobs />} />
-      <Route path="/drspage" element={<DrsPage />} />
-      <Route path="/calendar" element={<MyCalendar />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </Router>
-  )
+    <AuthProvider>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/drspage" element={<DrsPage />} />
+          <Route path="/calendar" element={<MyCalendar />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useContext } from "react";
+import listUsers from '../tempData/tablaUsuarios.json';
+export const AuthContext = createContext();
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within a AuthProvider");
+    }
+    return context;
+}
+export const AuthProvider = ({ children }) => {
+    const [users, setUsers] = useState(listUsers)
+    const [user, setUser] = useState(null);
+    const [isAutenticated, setIsAuthenticated] = useState(false);
+    const signUp = (newUser) => {
+        setUsers([...users, newUser]);
+        setUser(newUser);
+        signIn(newUser);
+    }
+    const signIn = (credentials) => {
+        const foundUser = listUsers.find(u => u.email === credentials.email && u.password === credentials.password);
+        if (foundUser) {
+            setUser(foundUser);
+            console.log(foundUser);
+            setIsAuthenticated(true);
+        } else {
+            alert("Usuario o contraseña incorrectos");
+            setUser(null);
+        }
+    }
+    return (
+        <AuthContext.Provider value={{signUp, user, signIn, isAutenticated}}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
