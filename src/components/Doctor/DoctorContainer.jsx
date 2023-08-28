57d132df8fc52a4c62a5bbe613c0b3a830097101
StillEffect6 <maxmora401@gmail.com>
2023-08-27 21:22:42 -0400

    avanzando

--- src/components/Doctor/DoctorContainer.jsx
+++ src/components/Doctor/DoctorContainer.jsx
@@ -3,14 +3,14 @@ import styles from './Doctor.module.css'
 
 export default function Doctor() {
     return (
-        <div className={styles.container}>
-            <p className={styles.p}>
-                FUNCIONOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-            </p>
-            <p className={styles.p}>
-                Cacha puedo cambiar los colores
-            </p>
-            
+        <div>
+            <aside className={styles.aside}>
+                <p className={styles.p}>a</p>
+
+            </aside>
+            <div className={styles.container}> 
+                <h2 className={styles.SecondTitle}>Doctora</h2>
+            </div>
         </div>
     )
 }
