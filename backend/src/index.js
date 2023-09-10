a1abc15e05b321fbed8c6744b7dbee9c8e8ad1d5
Vicente Rivera <vicenterivera225@gmail.com>
2023-09-09 23:19:40 -0300

    ahora si calendario

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import morgan from "morgan"
-
-const app = express()
-app.use(morgan("dev"))
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
