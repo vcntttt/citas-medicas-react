8f7c0b4caedbd1721708ab037c55db0cb175d23b
Juan Muñoz <juan.munoz.veloso@gmail.com>
2023-09-09 22:49:00 -0300

    especialidades colores

--- client/src/components/JobCard/Card.jsx
+++ client/src/components/JobCard/Card.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import styles from "./Card.module.css";
 
 export default function Card({item}) {
   return (
-    <div>
-      {item.JobTitle}
-    </div>
-  )
+    
+    <Link to= "/calendar">
+    <div className={styles.especialidad}>
+    <img className={styles.imgEsp}src="../../assets/especialidad.jpg" alt="Especialidad" />
+    <p className={styles.nomEsp}>{item.JobTitle}</p>
+    
+  </div></Link>
+  );
 }
