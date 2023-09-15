c2d3088abfa30d28e0daa8245df7aa5081a3dfbd
Maximo Mora <maxmora401@gmail.com>
2023-09-15 19:17:48 -0300

    login arreglado, faltan mejoras

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -19,41 +19,29 @@ export default function Login() {
     
   return (
     <div className={styles.container}>
-
       <div className={styles.registerMenu}>
-
-
         <div className={styles.containerTitle}>
           <h1 className={styles.title}>Login</h1>
         </div>
+        <form className={styles.form} action="" onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.containerData}>
+            <input {...register ("email", { required: true })} placeholder="Email" />
+            <input type="password" {...register ("password", { required: true })} placeholder="Password" />
+          </div>
+
+          <div className={styles.containerSubmit }>
+            <input className={styles.submit} type="submit" />
+          </div>
+          <div className={styles.containerRegister}>
+            <label >No tienes una cuenta? <Link className={styles.link} to="/register"><u>Registrate</u></Link> </label>
+          </div>
 
 
-          <form className={styles.containerForm} action="" onSubmit={handleSubmit(onSubmit)}>
-
-              <div className={styles.containerData}>
-                <input {...register ("email", { required: true })} placeholder="Email" />
-                <input type="password" {...register ("password", { required: true })} placeholder="Password" />
-              </div>
-
-
-              <div className={styles.containerSubmit}>
-                <input className={styles.submit} type="submit" />
-              </div>
-
-              <div className={styles.containerRegister}>
-                <label >No tienes una cuenta? <Link className={styles.link} to="/register"><u>Registrate</u></Link> </label>
-              </div>
-
-          </form>
- 
-
-
+        </form>
       </div>
-
       <div className={styles.imageContainer}>
         <img src="https://images.unsplash.com/photo-1579684385127-1ef15d508118?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80"/>
       </div>
-
     </div>
   )
 }
