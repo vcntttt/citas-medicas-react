eae09285047bc3ee825951d31596582ed47b42ae
Maximo Mora <maxmora401@gmail.com>
2023-09-18 03:04:23 -0300

    finalizado con el pagina 404

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,9 +1,28 @@
-import image from '../assets/404_not_found.jpg';
+import image from '../assets/404.jpg';
+import styles from '../styles/NotFound.module.css';
+import { Link, useNavigate } from "react-router-dom";
+
 export default function NotFound() {
   return (
-    <div>
+    <div className={styles.container}>
+      
       <img src={image}/>
       
+              
+      <div className={styles.menuContainer}>
+
+        <div className={styles.textContainer}>
+          <h1>No podemos encontrar la página que estás buscando</h1>
+          <p>regresa a la página de inicio apretando el boton de abajo</p>
+        </div>
+
+        <div className={styles.link}> 
+          <label ><Link className={styles.link} to="/">Pagina principal</Link> </label>
+
+
+        </div>
+      </div>
+
     </div>
   )
 }
