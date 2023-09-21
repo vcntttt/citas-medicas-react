87e084a5dadf6dd30ac25e370a8847b5a5e228a6
Juan Muñoz <juan.munoz.veloso@gmail.com>
2023-09-21 14:40:31 -0300

    estilo ProfilePage

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,5 +1,44 @@
+import styles from "../styles/Profile.module.css";
+
+
 export default function ProfilePaga() {
   return (
-    <div>ProfilePage</div>
+    <div>
+      <div className={styles.saludo}>
+        <p>¡Hola!</p>
+        <p>Nombre:</p>
+      </div>
+      <div className= {styles.contenedor}>
+        <div className={styles.titulo}>
+          <p> Tus horas</p>
+        </div>
+        <div className={styles.infoDiv}>
+          <p className={styles.info}>Especialidad:</p> <button className={styles.btnCambiar}>Cambiar hora</button>
+          <p className={styles.info}>Fecha:</p> <button className={styles.btnCambiar}>Cancelar hora</button>   
+          <p className={styles.info}>Doctor:</p> 
+        </div>
+      </div>
+
+      <div className= {styles.contenedor2}>
+        <div className={styles.datospersDiv}>
+          <p className={styles.titulo2}>Datos personales</p>
+          <p className={styles.datospers}>Nombre:</p> <button className={styles.btnModi}>Modificar</button>
+          <p className={styles.datospers}>Apellido:</p> <button className={styles.btnModi}>Modificar</button>
+          <p className={styles.datospers}>Direccion:</p> <button className={styles.btnModi}>Modificar</button>
+          <p className={styles.datospers}>Rut:</p> <button className={styles.btnModi}>Modificar</button>
+        </div>
+
+      <div className={styles.contenedor3}>
+        <div className={styles.datoscontacDiv}>
+          <p className={styles.titulo3}>Datos de Contacto</p> 
+          <p className={styles.datoscontac}>Telefono:</p> <button className={styles.btnModi}>Modificar</button>
+          <p className={styles.datoscontac}>Email:</p> <button className={styles.btnModi}>Modificar</button>
+          <p className={styles.datoscontac}>Contraseña:</p> <button className={styles.btnModi}>Modificar</button>
+        </div>
+
+      </div>
+
+      </div>
+    </div>
   )
 }
