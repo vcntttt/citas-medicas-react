f1f61ca263256d155641f7df9e5d78543c7ed414
Maximo Mora <maxmora401@gmail.com>
2023-10-09 20:05:38 -0300

    hola

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,9 +1,10 @@
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
+import styles from "./Input.module.css";
 
 export default function Input({type, errors, register }) {
     return (
         <div>
-            <input className='' type={type} placeholder={type === "password" ? "Contraseña" : "Email"} {...register(type, { required: true})}/>
+            <input className={styles.input} type={type} placeholder={type === "password" ? "Contraseña" : "Email"} {...register(type, { required: true})}/>
             {errors[type] && <p>Este campo es requerido</p>}
         </div>
     );
--- client/src/pages/AuthPages/LoginPage.jsx
+++ client/src/pages/AuthPages/LoginPage.jsx
@@ -1,57 +1,53 @@
 import styles from "../../styles/Login.module.css";
 import { useForm } from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom";
-import { Toaster} from "sonner";
+import { Toaster } from "sonner";
 import image from "../../assets/loginIMG.webp"
-import useAuthStore  from "../../store/authStore";
+import useAuthStore from "../../store/authStore";
 import useErrorHandler from "../../hooks/useErrors";
 import useIfAuth from "../../hooks/useIfAuth";
 import Input from "../../components/Input/Input.jsx";
 export default function Login() {
-    const {register, handleSubmit, formState: { errors }} = useForm();
-    const {signIn, errors: sigInErrors} = useAuthStore();
-    const navigate = useNavigate();
-    const onSubmit =handleSubmit((data) => {
-      signIn(data, navigate);
-    }) 
-    useIfAuth();
-    useErrorHandler(sigInErrors);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { signIn, errors: sigInErrors } = useAuthStore();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit((data) => {
+    signIn(data, navigate);
+  })
+  useIfAuth();
+  useErrorHandler(sigInErrors);
   return (
     <div className={styles.container}>
-      <div className={styles.registerMenu}>
-        <div className={styles.containerTitle}>
+
+      <div className={styles.user}>
+
+        <div className={styles.titleContainer}>
           <h1 className={styles.title}>Login</h1>
         </div>
+
         <form className={styles.form} action="" onSubmit={handleSubmit(onSubmit)}>
 
-          <div className={styles.containerData}>
-
-            <Input type="email" register={register} errors={errors}/>
-
-            <Input  type="password" register={register} errors={errors} />
-
+          <div className={styles.inputs}>
+            <Input type="email" register={register} errors={errors} />
+            <Input type="password" register={register} errors={errors} />
           </div>
-          <div className={styles.containerSubmit }>
-            <input className={styles.submit} type="submit" value="Entrar"/>
+
+          <div className={styles.containerSubmit}>
+            <input className={styles.submit} type="submit" value="Entrar" />
           </div>
-          <div className={styles.containerRegister}>
-            <label >No tienes una cuenta? <Link className={styles.link} to="/register">Registrate</Link> </label>
+
+          <div  className={styles.registerContainer} >
+            <label className={styles.register} >No tienes una cuenta? <Link to="/register">Registrate</Link> </label>
+            <Toaster duration={3000} toastOptions={{ style: { background: "red", color: "#fff", border: 0 } }} />
           </div>
-          <Toaster 
-          duration={3000}
-          toastOptions={{
-            style: {
-              background: "red",
-              color: "#fff",
-              border: 0
-            }
-          }}
-          />
+
         </form>
+
       </div>
-      <div className={styles.imageContainer}>
-        <img src={image}/>
-      </div>
+
+
+      <img className={styles.image} src={image} />
+
     </div>
   )
 }
