5ef212a0b9db195dc9bd80f781939500fe446619
Vicente Rivera <vicenterivera225@gmail.com>
2023-10-28 21:17:19 -0300

    ajuste rounded

--- client/src/pages/InfoPages/ProfilePage.jsx
+++ client/src/pages/InfoPages/ProfilePage.jsx
@@ -8,8 +8,7 @@ export default function ProfilePage() {
   return (
     <div className="grid grid-cols-2 min-h-[90vh] gap-4 p-4">
         <DatesBoard dates={userDates} className="col-span-1"/>
-
-      <div className="bg-gray-800 rounded-[20px] p-4">
+      <div className="bg-gray-800 rounded-lg p-4">
       <h1 className="text-white text-xl mb-4">¡Hola, {role}!</h1>
         <div className="bg-white p-4 rounded-[10px] mb-4">
           <p className="text-black text-xl font-bold mb-4">Datos personales</p>
@@ -24,8 +23,7 @@ export default function ProfilePage() {
             <Btn>Modificar</Btn>
           </div>
         </div>
-
-        <div className="bg-white p-4 rounded-[10px]">
+        <div className="bg-white p-4 rounded-lg">
           <p className="text-black text-xl font-bold mb-4">Datos de Contacto</p>
           <p className="text-2xl py-6"> Proximamente....</p>
         </div>
