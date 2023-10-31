ab87d31fdf4303e76a28d4a7bb0f83501b706854
Maximo Mora <maxmora401@gmail.com>
2023-10-31 13:04:23 -0300

    responsively we pageeeeeeeeeee

--- client/src/components/Home/Btn.jsx
+++ client/src/components/Home/Btn.jsx
@@ -1,7 +1,7 @@
 function Btn({children, onClick, styles}) {
   return (
     <button
-        className={`bg-onahau-500 w-full py-2 px-4 my-8  hover:bg-onahau-600  hover:text-white text-black  rounded ${styles}`}
+        className={`bg-onahau-500 w-full py-2 px-4 my-4  hover:bg-onahau-600  hover:text-white text-black  rounded ${styles}`}
         onClick={onClick}
     >{children}</button>
   )
