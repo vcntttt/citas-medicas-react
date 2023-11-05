25c4488b47ff101b64e5c5f33da11245476f5f76
Vicente Rivera <vicenterivera225@gmail.com>
2023-11-05 20:08:36 -0300

    como tan wn pato

--- client/src/components/Cards/Especialidad.jsx
+++ client/src/components/Cards/Especialidad.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import useAuthStore from "../../store/authStore";
 import { useNavigate } from "react-router-dom";
 export default function Especialidad({nombre}) {
-  const {isAuthenticated, userHasData} = useAuthStore();
+  const {isAuthenticated, user} = useAuthStore();
   const navigate = useNavigate();
   return (
     <div 
     className="bg-onahau-500 p-2 rounded-sm text-black flex flex-col items-center py-4">
       <h1 className="text-current py-4 text-xl">{nombre}</h1>
       {isAuthenticated ? (
-        <button onClick={() => userHasData ? navigate(`/calendar/${nombre}`) : navigate("/formulary")}
+        <button onClick={() => (user?.nombre && user?.apellido) ? navigate(`/calendar/${nombre}`) : navigate("/formulary")}
         className="bg-slate-900 hover:bg-greencus-500 text-white px-2 py-1 rounded">
           Pedir Hora
         </button>
