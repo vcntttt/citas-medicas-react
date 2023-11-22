a03c1b57261510bbd24ea9e32133e7d67a13cdee
Maximo Mora <maxmora401@gmail.com>
2023-11-22 12:48:28 -0300

    login y register respon

--- client/src/pages/AuthPages/LoginPage.jsx
+++ client/src/pages/AuthPages/LoginPage.jsx
@@ -29,13 +29,14 @@ export default function Login() {
   return (
     <div className="flex md:flex items-center justify-center h-screen bg-[#2E3238]">
 
-      <div className="h-4/6 md:h-3/6  lg:h-4/5 lg:w-2/6 rounded-lg lg:shadow-2xl">
+      <div className="w-full h-4/6 md:h-3/6  lg:h-4/5 lg:w-2/6 rounded-lg lg:shadow-2xl">
+
         <div className="w-full h-full">
           <div className="flex h-1/4 items-center justify-center md: md:h-1/6">
             <h1 className="text-white text-center text-3xl">Login</h1>
           </div>
           <form className="h-3/4" action="" onSubmit={handleSubmit(onSubmit)}>
-            <div className="h-2/6 justify-center text-center md:h-2/6 lg:h-2/6">
+            <div className="bg-[#2E3238]  h-3/6 justify-center text-center md:h-2/6 lg:h-2/6">
               <Input type="email" placeholder="Email" register={register} errors={errors} />
               <Input type="password" placeholder="Contraseña" register={register} errors={errors} />
             </div>
@@ -50,7 +51,7 @@ export default function Login() {
           </form>
         </div>
       </div>
-        <img className="w-auto md:h-3/6 md:flex md:ml-4 lg:h-4/5  hidden" src={image} />
+        <img className="w-auto md:h-3/6 md:flex md:ml-3 lg:h-4/5  hidden" src={image} />
     </div>
   )
 }
--- client/src/pages/AuthPages/RegisterPage.jsx
+++ client/src/pages/AuthPages/RegisterPage.jsx
@@ -28,20 +28,20 @@ export default function Register() {
   useIfAuth();
 
   return (
-    <div className="flex flex-col-reverse h-screen w-screen md:flex-row items-center justify-center ">
+    <div className="flex flex-col-reverse h-screen w-screen md:flex-row items-center justify-center  bg-[#2E3238]">
 
-      <img className="w-auto md:h-3/6 lg:h-4/5" src={image} />
+      <img className="hidden md:block w-auto md:h-3/6 md:mr-3 md:ml-3 lg:h-4/5 " src={image} />
 
-      <div className="h-4/6 w-full  items-center   md:h-3/6 lg:h-4/5 lg:w-2/6">
+      <div className="h-4/6 w-full  items-center   md:h-3/6 ml:lg:shadow-2xl lg:h-4/5 lg:w-2/6 lg:shadow-2xl">
 
-        <div className="bg-[#2E3238] w-6/6 md:bg-[#2E3238] h-full   lg:bg-[#2E3238]">
+        <div className="  w-6/6 md:bg-[#2E3238] h-full   lg:bg-[#2E3238]">
           
           <div className="flex h-1/4 items-center justify-center md:h-1/6 lg:lg:bg-[#2E3238] ">
             <h1 className=" text-white text-center text-3xl">Registro</h1>
           </div>
           <form className=" h-3/4" action="" onSubmit={handleSubmit(onSubmit)}>
 
-            <div className="flex-col h-3/6  ">
+            <div className=" flex-col h-3/6  ">
               <Input type="email" placeholder="Email" register={register} errors={errors} />
 
               <Input type="password" placeholder="Contraseña" register={register} errors={errors} />
@@ -56,7 +56,7 @@ export default function Register() {
               <input className="w-[200px] h-[50px] bg-[#55CCC9] hover:cursor-pointer hover:bg-[#46a4a1]" type="submit" value="Entrar" />
             </div>
 
-            <div className=" h-1/6 flex justify-center  md:h-2/6 items-center  my-0                                       ">
+            <div className=" h-1/6 flex justify-center text-center md:text-xs  md:px-5 md:h-2/6 items-center  my-0    lg:text-lg                                    ">
               <label className="text-[white]"  >Ya tienes una cuenta? <Link className="no-underline" to="/login"><u>Inicia Sesion</u></Link> </label>
             </div>
 
